a4c394db39860996b28a348b3312194e91b75127
FeRRRiJane <103585285+FeRRRiJane@users.noreply.github.com>
2023-10-04 19:28:29 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+$(function (){
+     $(document).ready(function(){
+        $('.top__slider').slick({
+          dots: true,
+          arrows: false,
+          fade: true,
+          autoplay: true
+       });
+        });
+})
